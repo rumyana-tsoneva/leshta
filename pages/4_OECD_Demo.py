29292c3bdb26555d8a90cba209f5453221c8745a
rumyana-tsoneva <76905959+rumyana-tsoneva@users.noreply.github.com>
2023-11-02 14:55:09 +0000

    oecd demo

--- pages/4_OECD_Demo.py
+++ pages/4_OECD_Demo.py
@@ -6,17 +6,22 @@ Here's our first attempt at using data to create a table:
 import streamlit as st
 import pandas as pd
 import altair as alt
+from cif import cif
 
-df = pd.DataFrame({
-  'A': [1, 2, 3, 4],
-  'B': [10, 20, 30, 40]
-})
-chart = (alt.Chart(df)
+data, subjects, measures = cif.createOneCountryDataFrameFromOECD(country= 'ESP', dsname='MEI')
+
+st.write("### Subjects ", subjects.sort_index())
+
+ipc = data['CPHP0702']['IXOB'].reset_index()
+
+ipc_chart = (alt.Chart(ipc)
         .mark_area(opacity=0.3)
         .encode(
-            x="A:T",
-            y=alt.Y("B:Q", stack=None),
+            x="time:T",
+            y=alt.Y("IXOB:Q", stack=None),
             color="Region:N",
         )
 )
-st.altair_chart(chart, use_container_width=True)
+st.altair_chart(ipc_chart, use_container_width=True)
+
+st.line_chart(data=ipc, x = 'time', y = 'IXOB', use_container_width=True)
